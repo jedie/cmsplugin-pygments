221952a426dc9e6b92bcfe80029b5afdcffc7a68
JensDiemer <git@jensdiemer.de>
2018-02-23 19:03:14 +0100

    Add DocString for #3

--- cmsplugin_pygments/models.py
+++ cmsplugin_pygments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from cms.models import CMSPlugin
 from pygments.styles import get_all_styles
-from pygments.lexers import get_all_lexers 
+from pygments.lexers import get_all_lexers
 
 STYLE_CHOICES = map(lambda x: (x,x), get_all_styles())
 
@@ -9,7 +9,12 @@ LANGUAGE_CHOICES = list(map(lambda x: (x[1][0], x[0]), get_all_lexers()))
 LANGUAGE_CHOICES.sort(key=lambda x: x[0])
 
 class PygmentsPlugin(CMSPlugin):
+    # It's a bad idea to set language/style choices here
+    # What happens if styles/lexers change in pygments?
+    # see: https://github.com/jedie/cmsplugin-pygments/issues/3
     code_language = models.CharField(max_length=20, choices=LANGUAGE_CHOICES)
     code = models.TextField()
     style = models.CharField(max_length=255, choices=STYLE_CHOICES)
     linenumbers = models.BooleanField(default=True)
+
+
